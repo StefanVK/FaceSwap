5f8696a4dee1b28777ade31a421bfdb687f3bf05
Stefan van Kessel <van_kessel@freenet.de>
2021-12-22 22:31:42 +0100

    windeployqt: try to handle both the vcpkg case with windeployqt.debug.bat and the normal install case without debug/release split

--- cmake/windeployqt.cmake
+++ cmake/windeployqt.cmake
@@ -5,15 +5,22 @@ find_package(Qt6 COMPONENTS Core REQUIRED)
 
 get_target_property(_qmake_executable Qt6::qmake IMPORTED_LOCATION)
 get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
+get_filename_component(_qt_windeployqt_dir "${_qmake_executable}" DIRECTORY)
+
+find_program(WINDEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
 
 function(windeployqt target)
-
     # POST_BUILD step
     # - after build, we have a bin/lib for analyzing qt dependencies
     # - we run windeployqt on target and deploy Qt libs
     IF(CMAKE_BUILD_TYPE MATCHES "Debug")
+        find_program(WINDEPLOYQT_DEBUG windeployqt.debug.bat HINTS "${_qt_bin_dir}")
+        if(WINDEPLOYQT_DEBUG)
+          set(WINDEPLOYQT_EXECUTABLE = ${WINDEPLOYQT_DEBUG})
+        endif()
+        message(WINDEPLOYQT_EXECUTABLE)
         add_custom_command(TARGET ${target} POST_BUILD
-            COMMAND "${_qt_bin_dir}/windeployqt.debug.bat"         
+            COMMAND ${WINDEPLOYQT_EXECUTABLE}        
                 --verbose 1
                 --debug
                 \"$<TARGET_FILE:${target}>\"
@@ -21,7 +28,7 @@ function(windeployqt target)
         )
     ELSE()
         add_custom_command(TARGET ${target} POST_BUILD
-            COMMAND "${_qt_bin_dir}/windeployqt.exe"         
+            COMMAND ${WINDEPLOYQT_EXECUTABLE}        
                 --verbose 1
                 --release
                 \"$<TARGET_FILE:${target}>\"
